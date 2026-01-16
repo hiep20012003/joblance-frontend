964319ffd271faa97fabeafae68d913a0ea80816
Hiep Nguyen <nguyendunghiep20012003@gmail.com>
2026-01-16 11:32:04 +0700

    fix: language date-fns

--- src/lib/middlewares/withCurrentPath.ts
+++ src/lib/middlewares/withCurrentPath.ts
@@ -1,19 +1,15 @@
-import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
-import { isSellerPath } from '@/lib/utils/helper';
-import { ChainableMiddleware } from '@/lib/utils/chain';
-import { LOGOUT } from '@/lib/constants/routes';
+import {NextFetchEvent, NextRequest} from 'next/server';
+import {NextResponse} from 'next/server';
+import {isSellerPath} from '@/lib/utils/helper';
+import {ChainableMiddleware} from "@/lib/utils/chain";
+import {LOGOUT} from "@/lib/constants/routes";
 
 export function withCurrentPath(middleware: ChainableMiddleware) {
     return async (req: NextRequest, event: NextFetchEvent, res: NextResponse) => {
         const response = res || NextResponse.next();
-        const { pathname } = req.nextUrl;
+        const {pathname} = req.nextUrl;
 
-        const isPrefetch =
-            req.headers.get('purpose') === 'prefetch' ||
-            req.headers.get('next-router-prefetch') === '1';
-        const isPageRequest = req.headers.get('accept')?.includes('text/html');
-
-        if (!isPrefetch && isPageRequest && !pathname.startsWith(LOGOUT)) {
+        if (!pathname.startsWith(LOGOUT)) {
             response.cookies.set('currentClientPath', pathname, {
                 path: '/',
                 sameSite: 'lax',
@@ -25,7 +21,7 @@ export function withCurrentPath(middleware: ChainableMiddleware) {
             const shouldBeSeller =
                 isSellerPage || (pathname.startsWith('/orders') && prevMode === 'seller');
 
-            response.cookies.set('mode', shouldBeSeller ? 'seller' : 'buyer', {
+            response?.cookies.set('mode', shouldBeSeller ? 'seller' : 'buyer', {
                 path: '/',
                 sameSite: 'lax',
                 maxAge: 60 * 60 * 24,
